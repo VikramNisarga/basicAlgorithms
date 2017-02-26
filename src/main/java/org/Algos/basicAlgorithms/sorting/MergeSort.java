d19eecaf93f7474e020eb94e3d62382c46d7548a
Vikram <vikramnisarga1@gmail.com>
2017-02-26 17:19:06 +0530

    changed the implementation of merge sort

--- src/main/java/org/Algos/basicAlgorithms/sorting/MergeSort.java
+++ src/main/java/org/Algos/basicAlgorithms/sorting/MergeSort.java
@@ -1,68 +1,83 @@
 package org.Algos.basicAlgorithms.sorting;
 
 public class MergeSort {
-	int[] array;
-	int[] helper;
-	int[] Array=new int[11];
-	public MergeSort(int[] array) {
-		super();
-		this.array=array;
-		this.helper=array;
+	int[] inputArray;
+
+
+	public int[] sort(int[] inputArray) {
+		int startIndex = 0;
+		int endIndex = inputArray.length - 1;
+		this.inputArray = inputArray;
+		return sortAndCount(startIndex, endIndex);
+
 	}
 
-	void recursiveArray(int firstPos,int lastPos) {
-		if(firstPos<lastPos){
-			int midPos=(firstPos+lastPos)/2;
-			System.out.println(firstPos+"   "+midPos+"   "+lastPos+"  inside Recursive method");
-			System.out.println('\n');
-			recursiveArray(firstPos, midPos);
-			recursiveArray(midPos+1, lastPos);
-			mergeArray(firstPos, midPos, lastPos);
-			
+	public int[] sortAndCount(int startIndex, int endIndex) {
+		if ((endIndex - startIndex) < 1) {
+			return new int[] { inputArray[startIndex] };
+		} else {
+			int leftHalfStartIndex = startIndex;
+			int leftHalfEndIndex = (startIndex + endIndex) / 2;
+
+			int rightHalfStartIndex = leftHalfEndIndex + 1;
+			int rightHalfEndIndex = endIndex;
+
+			return mergeAndCountSplit(sortAndCount(leftHalfStartIndex,
+					leftHalfEndIndex), sortAndCount(rightHalfStartIndex,
+					rightHalfEndIndex));
+
 		}
+
 	}
 
-	void mergeArray(int firstPos,int midPos,int lastPos){
-		int fPos=firstPos;
-		
-		int mPos=midPos+1;
-		
-		int lPos=lastPos;
-		
-		
+	public int[] mergeAndCountSplit(int[] leftHalf, int[] rightHalf) {
+		int leftLength = leftHalf.length;
+		int rightLength = rightHalf.length;
 
-		for (int i = fPos; i <midPos+1; i++) {
+		// length of sorted array is sum of both the arrays
+		int[] sortedArray = new int[leftLength + rightLength];
 
-		
-			if(array[fPos]<array[mPos]){	
-				Array[i]=array[fPos];
+		int leftCursor = 0, rightCursor = 0;
 
-				
-				fPos++;
+		for (int i = 0; i < leftLength + rightLength; i++) {
+			if (leftHalf[leftCursor] > rightHalf[rightCursor]) {
+				sortedArray[i] = rightHalf[rightCursor];
+				rightCursor++;
+
+
+				/*
+				 * if right_array is exhausted, append all the remaining
+				 * elements from left array to the sorted_array
+				 */
+				if (rightCursor >= rightLength) {
+					for (int l = leftCursor; l < leftLength; l++) {
+						// Increment the index of the sorted_array before
+						// appending
+						i++;
+						sortedArray[i] = leftHalf[l];
+					}
+					break;
+				}
+
+			} else {
+				sortedArray[i] = leftHalf[leftCursor];
+				leftCursor++;
+				/*
+				 * if left_array is exhausted, append all the remaining elements
+				 * from right array to the sorted_array
+				 */
+				if (leftCursor >= leftLength) {
+					for (int l = rightCursor; l < rightLength; l++) {
+						// increment the index of the sorted_array before
+						// appending
+						i++;
+						sortedArray[i] = rightHalf[l];
+					}
+					break;
+				}
 			}
-			else{
-				
-				System.out.println("else");
-				Array[i]=array[mPos];
-				Array[mPos]=array[fPos];
-				mPos++;
-			}
 
 		}
-		while(fPos<=mPos){
-			int i = midPos;
-			Array[i]=array[fPos];
-			fPos++;
-			i++;
-		}
-
-	}
-	public static void main(String[] args) {
-		int[] array={4,1,5,3};
-		MergeSort m=new MergeSort(array);
-		m.recursiveArray(0, 3);
-		for (int i = 0; i < 4; i++) {
-			System.out.print(m.Array[i] + " ");
-		}
+		return sortedArray;
 	}
 }
